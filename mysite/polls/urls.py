570a87f949251f3a1a8f541af508fe53632bfbc1
guanfeix <guanfeix@gmail.com>
2018-04-18 18:05:17 +0800

    3 cmt

---
+++ mysite/polls/urls.py
@@ -0,0 +1,37 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    # path('', views.index, name='index'),
+    # # ex: /polls/5/
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # # ex: /polls/5/results/
+    # path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('about/', TemplateView.as_view(template_name="polls/about.html")),
+]
