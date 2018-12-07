2b3eeac32da19c31b54419e773b3b4a9dbd2c05f
guanfeix <guanfeixiang@qccr.com>
2018-12-07 10:50:40 +0800

    ll

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cl&u$ezoh!!u++h-emr4a^k40)q7z($xs#o-j@wq(vk^c%@gre'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
